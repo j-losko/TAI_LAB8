c86548728a20bab3b013137bb1c1fdb4580a3e7c
j-losko <43851895+j-losko@users.noreply.github.com>
2019-04-05 16:57:59 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {BlogHomeComponent} from "./blog-home/blog-home.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {HomeComponent} from "./components/home/home.component";
+import {QuizComponent} from "./components/quiz/quiz.component";
+import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogHomeComponent,
+
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent
+  }  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
